ccdec1a9c65cfa3cf031963db8bacd14cfab9c37
Desislava Belcheva <72351356+dsbelcheva@users.noreply.github.com>
2024-02-11 19:43:20 +0200

    Some chanegs to Mystoc Blocks game

--- Mystic Blocks/setup.py
+++ Mystic Blocks/setup.py
@@ -9,7 +9,7 @@ setup(
     ],
       entry_points={
         'console_scripts': [
-            'play-mystic-game=play_mystic_game:cli',
+            'play-mystic-blocks=play_mystic_game:cli',
         ],
     },
 )
